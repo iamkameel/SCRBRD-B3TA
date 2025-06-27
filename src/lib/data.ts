e38d9f1d83a353908ffccbc51aab1f3c1c12a5c9
Kameel Kalyan <kameel@maverickdesign.co.za>
2025-06-27 23:53:51 +0000

    next step

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -181,6 +181,16 @@ export interface PlayerStats {
     stumpings: number;
 }
 
+export interface TeamStats {
+    matchesPlayed: number;
+    matchesWon: number;
+    matchesLost: number;
+    matchesDrawn: number;
+    totalRunsScored: number;
+    totalWicketsTaken: number;
+    netRunRate: number;
+}
+
 export interface PlayerTeamAssignment {
   teamId: string;
   teamName: string;
