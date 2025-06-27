8ffd088740c24a47254839f19cf544a2e8d9739a
Kameel Kalyan <kameel@maverickdesign.co.za>
2025-06-27 23:10:35 +0000

    next step

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCVBkWe3A7AwJdk_J6yx7sBhOgu4nWg9Ik",
-  authDomain: "scrbrd-beta-2.firebaseapp.com",
-  projectId: "scrbrd-beta-2",
-  storageBucket: "scrbrd-beta-2.appspot.com", // Corrected for SDK compatibility
-  messagingSenderId: "489561247753",
-  appId: "1:489561247753:web:a09285d3776fde63f028d8",
-  measurementId: "G-28ETF9TDYZ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
