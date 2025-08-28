f7558843e7b7d4519d1e013f705f5a65f314eb83
Kameel Kalyan <kameel@maverickdesign.co.za>
2025-08-28 11:56:16 +0000

    there are 3 different Naidoo's in the Durban High School squad. How do w

--- src/lib/actions/analysis.ts
+++ src/lib/actions/analysis.ts
@@ -1,5 +1,4 @@
 
-
 'use server';
 
 import { revalidatePath } from 'next/cache';
@@ -380,4 +379,3 @@ export async function generateHighlightReelAction(matchId: string): Promise<High
         throw new Error("The AI failed to generate highlights.");
     }
 }
-
