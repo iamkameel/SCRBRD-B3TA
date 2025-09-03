01af875f55a8ebcd2aca03dd722871c0015dcca4
Kameel Kalyan <kameel@maverickdesign.co.za>
2025-09-03 08:55:37 +0000

    Error: Map container is already initialized.     at NewClass._initContai

--- src/app/fields/[fieldId]/client.tsx
+++ src/app/fields/[fieldId]/client.tsx
@@ -1,7 +1,7 @@
+
 'use client';
 
 import * as React from "react";
-import dynamic from 'next/dynamic';
 import Link from 'next/link';
 import Image from 'next/image';
 import { format } from "date-fns";
