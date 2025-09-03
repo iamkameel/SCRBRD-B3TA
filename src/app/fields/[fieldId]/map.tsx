153325578b1018c064abf8841fa0e838da4eb7bd
Kameel Kalyan <kameel@maverickdesign.co.za>
2025-09-03 10:24:32 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/fields/[fieldId]/map.tsx
+++ src/app/fields/[fieldId]/map.tsx
@@ -1,10 +1,10 @@
-
 'use client';
 
 import * as React from 'react';
 import 'leaflet/dist/leaflet.css';
 import type { LatLngExpression } from 'leaflet';
 import L from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 // Leaflet's icon URLs don't work well with bundlers, so we manually fix them.
 import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
@@ -22,12 +22,6 @@ const DefaultIcon = L.icon({
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
-
-// We dynamically import leaflet components to ensure they only run on the client
-const MapContainer = React.lazy(() => import('react-leaflet').then(m => ({ default: m.MapContainer })));
-const TileLayer = React.lazy(() => import('react-leaflet').then(m => ({ default: m.TileLayer })));
-const Marker = React.lazy(() => import('react-leaflet').then(m => ({ default: m.Marker })));
-const Popup = React.lazy(() => import('react-leaflet').then(m => ({ default: m.Popup })));
 
 interface FieldMapProps {
     center: LatLngExpression;
@@ -47,16 +41,14 @@ export default function FieldMap({ center, popupText }: FieldMapProps) {
     }
     
     return (
-        <React.Suspense fallback={<div className="h-full w-full bg-muted animate-pulse rounded-lg" />}>
-            <MapContainer center={center} zoom={15} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={center}>
-                    <Popup>{popupText}</Popup>
-                </Marker>
-            </MapContainer>
-        </React.Suspense>
+        <MapContainer center={center} zoom={15} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={center}>
+                <Popup>{popupText}</Popup>
+            </Marker>
+        </MapContainer>
     );
 }
