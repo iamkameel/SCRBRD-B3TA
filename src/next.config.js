77961551ea993df949c06d777387f047500071be
Kameel Kalyan <kameel@maverickdesign.co.za>
2025-10-27 17:31:49 +0000

    fix the error

---
+++ src/next.config.js
@@ -0,0 +1,32 @@
+/** @type {import('next').NextConfig} */
+const withPWAInit = require('@ducanh2912/next-pwa').default;
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig = {
+  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ['genkit'],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'placehold.co', port: '', pathname: '/**' },
+      { protocol: 'https', hostname: 'firebasestorage.googleapis.com', port: '', pathname: '/**' },
+      { protocol: 'https', hostname: 'maverickdesign.co.za', port: '', pathname: '/**' },
+      { protocol: 'https', hostname: 'firebasestudio-hosting.web.app', port: '', pathname: '/**' },
+      { protocol: 'https', hostname: 'picsum.photos', port: '', pathname: '/**' },
+      { protocol: 'https://tile.openstreetmap.org', port: '', pathname: '/**' },
+    ],
+  },
+};
+
+module.exports = withPWA(nextConfig);
