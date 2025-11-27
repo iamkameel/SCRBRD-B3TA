7c43275fd55f87f127f2d6fca9be384214131067
Kameel Kalyan <kameel@maverickdesign.co.za>
2025-11-27 13:42:04 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,7 +12,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCVBkWe3A7AwJdk_J6yx7sBhOgu4nWg9Ik",
   authDomain: "scrbrd-beta-2.firebaseapp.com",
   projectId: "scrbrd-beta-2",
-  storageBucket: "scrbrd-beta-2.firebasestorage.app",
+  storageBucket: "scrbrd-beta-2.appspot.com",
   messagingSenderId: "489561247753",
   appId: "1:489561247753:web:a09285d3776fde63f028d8",
   measurementId: "G-28ETF9TDYZ"
