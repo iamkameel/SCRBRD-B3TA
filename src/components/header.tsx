fad81d4d67e9cb369c579061b56eec71c855889a
Kameel Kalyan <kameel@maverickdesign.co.za>
2025-11-30 18:14:24 +0000

    there's no option to select a dropdown in the header to switch roles.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -245,7 +245,7 @@ export function Header() {
                                             <AccordionContent className="pt-1 pb-0 pl-8">
                                                 <div className="flex flex-col gap-1">
                                                     {visibleItems.map((item) => {
-                                                        const isActive = pathname.startsWith(item.href.split('?')[0]);
+                                                        const isActive = pathname.startsWith(item.href);
                                                         return (
                                                             <SheetClose asChild key={item.label}>
                                                                 <Link
