ae2c8240e075732c7edddf105aa917e39e4b0bd0
Kameel Kalyan <kameel@maverickdesign.co.za>
2025-12-03 14:49:27 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: isPendi

--- src/app/dashboards/admin-dashboard.tsx
+++ src/app/dashboards/admin-dashboard.tsx
@@ -347,10 +347,11 @@ export default function AdminDashboard() {
             <FieldDialog mode="add" open={dialogState.field} onOpenChange={(open) => setDialogState(s => ({...s, field: open}))} schools={dialogData.schools} groundkeepers={dialogData.groundskeeper} />
             <TransactionDialog mode="add" open={dialogState.financial} onOpenChange={(open) => setDialogState(s => ({...s, financial: open}))} />
             <SponsorDialog mode="add" open={dialogState.sponsor} onOpenChange={(open) => setDialogState(s => ({...s, sponsor: open}))} />
-            {isPending && <p>Loading...</p>}
+            {loading && <p>Loading...</p>}
             
         </>
     )}
     </>
   );
 }
+
